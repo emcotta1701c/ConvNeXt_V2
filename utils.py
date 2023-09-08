d9edee4e59e9f4b0c96cb10096716acb7970d66a
emcotta1701c <141988532+emcotta1701c@users.noreply.github.com>
2023-09-07 22:12:06 -0700

    inserted strict parameter to load_state_dict (Pytorch call, not custom call)

--- utils.py
+++ utils.py
@@ -494,7 +494,7 @@ def auto_load_model(args, model, model_without_ddp, optimizer, loss_scaler, mode
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
 
-        model_without_ddp.load_state_dict(checkpoint['model'], autoencoder=autoencoder)
+        model_without_ddp.load_state_dict(checkpoint['model'], strict=not autoencoder)
         print("Resume checkpoint %s" % args.resume)
         if 'optimizer' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
